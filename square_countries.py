412d643e5dcc095551b357a421d398d06478bdcb
Serge Johanns <serge.johanns@gmail.com>
2024-03-21 03:49:57 +0100

    Add command line arguments

--- square_countries.py
+++ square_countries.py
@@ -1,4 +1,6 @@
 import json
+import argparse
+import pathlib
 import scipy
 import geojson
 from shapely.geometry import shape
@@ -7,14 +9,6 @@ import matplotlib.pyplot as plt
 from alive_progress import alive_it
 
 from shapes.square import Square
-
-SHAPES_FILE = "country_shapes.geojson"
-
-with open(SHAPES_FILE) as f:
-    geometric_shapes = {
-        geo_shape["properties"]["cntry_name"]: shape(geo_shape["geometry"])
-        for geo_shape in geojson.load(f)["features"]
-    }
 
 
 def error_function(target_shape, country_shape):
@@ -47,14 +41,16 @@ def optimize(country_shape):
     return optimal_parameters.x, optimal_parameters.fun
 
 
-def calculate_scores(target_countries=list(geometric_shapes.keys())):
+def calculate_scores(countries, target_countries=None):
+    if target_countries is None:
+        target_countries = countries.keys()
     scores = []
     bar = alive_it(target_countries)
 
     for country_name in bar:
-        shape = geometric_shapes[country_name]
+        shape = countries[country_name]
         bar.text = country_name
-        optimal_square, score = optimize(geometric_shapes[country_name])
+        optimal_square, score = optimize(countries[country_name])
         scores.append(
             {
                 "country": country_name,
@@ -67,12 +63,76 @@ def calculate_scores(target_countries=list(geometric_shapes.keys())):
     return scores
 
 
-target_countries = list(geometric_shapes.keys())[:10]
-scores = calculate_scores(target_countries)
-for item in scores:
-    country = item["country"]
-    plot_polygon(geometric_shapes[country], add_points=False)
-    square = Square.from_parameters(item["parameters"])
-    plot_polygon(square, color="red", add_points=False)
-    plt.show()
-print(json.dumps(scores))
+def get_country_shapes(file):
+    with file as f:
+        return {
+            geo_shape["properties"]["cntry_name"]: shape(geo_shape["geometry"])
+            for geo_shape in geojson.load(f)["features"]
+        }
+
+
+def country_to_filename(country_name):
+    return f"{country_name.lower().replace(' ', '_')}.png"
+
+
+def write_report(report_output, scores):
+    report_output.mkdir(exist_ok=True, parents=True)
+    for item in scores:
+        country = item["country"]
+        plot_polygon(countries[country], add_points=False)
+        square = Square.from_parameters(item["parameters"])
+        plot_polygon(square, color="red", add_points=False)
+        plt.savefig(report_output / f"{country_to_filename(country)}")
+        plt.clf()
+    with open(args.report_output / "report.md", "w") as f:
+        f.write("# Country Shape Tester\n")
+        f.write("## Countries\n")
+        f.write("The following countries were tested:\n")
+        for item in scores:
+            f.write(f"- {item['country']}\n")
+        f.write("\n")
+        f.write("## Results\n")
+        f.write(
+            "The following table shows the scores and optimal shape parameters for each country:\n"
+        )
+        f.write("| Country | Error | Image |\n")
+        f.write("|---------|-------|------------|\n")
+        for item in scores:
+            f.write(
+                f"| {item['country']} | {item['score']:.2%} | ![{item['country']}]({country_to_filename(item['country'])}) |\n"
+            )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Country Shape Tester",
+        description="Program to test which country is best approximated by a certain shape.",
+    )
+    parser.add_argument(
+        "--report-output",
+        help="Path to write the markdown report to.",
+        type=pathlib.Path,
+        default="report",
+    )
+    parser.add_argument(
+        "--json-output",
+        help="File to write the resulting scores and optimal shape parameters to.",
+        type=argparse.FileType("w", encoding="latin-1"),
+    )
+    parser.add_argument(
+        "--country-file",
+        help="GeoJSON file containing the country shapes.",
+        default="country_shapes.geojson",
+        type=open,
+    )
+    args = parser.parse_args()
+
+    countries = get_country_shapes(args.country_file)
+    target_countries = list(countries.keys())[:10]
+    scores = calculate_scores(countries, target_countries)
+
+    write_report(args.report_output, scores)
+
+    if args.json_output:
+        with args.json_output as f:
+            json.dump(scores, f, indent=4)
